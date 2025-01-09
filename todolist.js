d28bf2f868dec2205c49b98ab6786e4c19d3c6a3
quang211214006@lms.utc.edu.vn dangquang1 <nq65383@gmail.com>
2025-01-09 17:51:53 +0700

    done action add

---
+++ todolist.js
@@ -0,0 +1,104 @@
+//validate
+function validate(option) {
+    var formElement = document.querySelector(option.form);
+    if (formElement) {
+        option.rules.forEach(function (rule) {
+            var inputElement = formElement.querySelector(rule.selector);
+            var errorelemnt=formElement.parentElement.querySelector('small')
+            var btnsave=formElement.querySelector('.btn-primary')
+            btnsave.disabled=true;
+            if (inputElement) {
+                inputElement.onblur = function () {
+                    var errorMessage = rule.test(inputElement.value);
+                    if(errorMessage){
+                        errorelemnt.innerText=errorMessage;
+                        btnsave.disabled=true;
+                    }
+                    else
+                    {
+                        errorelemnt.innerText = '';
+                        btnsave.disabled = false;
+                    }
+                }
+                
+            }
+        });
+    }
+}
+
+
+validate.isrequired = function (selector) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.trim() ? undefined : 'Please enter your task';
+        }
+    };
+};
+
+// thêm
+function showdata()
+{
+    var tasklist;
+    var taskvalue=document.querySelector('#taskid')
+    
+    if(localStorage.getItem("tasklist")==null){
+        tasklist=[];
+    }
+    else {
+        tasklist=JSON.parse(localStorage.getItem("tasklist"))
+    }
+
+    var table = "";
+    tasklist.forEach(function (el, index) {
+        table += `
+            <tr>
+                <td>${index ++}
+                <td>${el.taskvalue}</td> 
+                <td>${false}</td> 
+                <td>
+                    <button type="button" class="btn btn-danger" onclick="onDelete(${index})">Delete</button>
+                    <button type="button" class="btn btn-success ms-1" onclick="onFinished(${index})">Finished</button>
+                    <button type="button" class="btn btn-edit" onclick="onEdit(${index})">Edit</button>
+                </td>
+            </tr>`;
+    });
+    document.querySelector('.data-table tbody').innerHTML=table;
+    
+}
+document.onload=showdata();
+function addtask() {
+    var inputvalue = document.querySelector('#taskid').value.trim();
+    var disabledbtn = document.querySelector('.btn-primary');
+    var tasklist;
+    if (localStorage.getItem("tasklist") == null) {
+        tasklist = [];
+    } else {
+        tasklist = JSON.parse(localStorage.getItem("tasklist"));
+    }
+    var exists = tasklist.some(function (item) {
+        return item.taskvalue === inputvalue;
+    });
+
+    if (exists) {
+        console.log('Đã tồn tại');
+    } else {
+        console.log('Chưa tồn tại');
+        tasklist.push({
+            taskvalue: inputvalue,
+        });
+    }
+        localStorage.setItem("tasklist", JSON.stringify(tasklist));
+        showdata();
+    document.getElementById("taskid").value = "";
+    disabledbtn.disabled = true;
+}
+
+validate(
+    {
+        form:'#form',
+        rules:[
+        validate.isrequired("#taskid")
+       ]
+    }
+)
